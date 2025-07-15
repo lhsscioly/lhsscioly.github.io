f6c46ce1660438704cce3fe854c8d72c0afded39
shubhvarshney <shubh.varshney@lhswildcats.org>
2025-07-14 22:44:39 -0500

    Changed Router

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
-import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 import "./index.css";
 
 createRoot(document.getElementById("root")).render(
