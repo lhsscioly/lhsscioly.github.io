4a691511a0da9c88e750a663d45a263e997aef52
shubhvarshney <shubh.varshney@lhswildcats.org>
2025-07-14 21:33:56 -0500

    Testing deploy process

--- scioly-frontend/src/services/login.js
+++ scioly-frontend/src/services/login.js
@@ -1,9 +1,9 @@
-import axios from "axios";
+import apiClient from "./apiClient";
 
 const baseUrl = "/api/login";
 
 const login = async (credentials) => {
-  const response = await axios.post(baseUrl, credentials);
+  const response = await apiClient.post(baseUrl, credentials);
   return response.data;
 };
 
